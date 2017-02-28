334c807bd5ca4c96e302f9b7a131f897d0c65d0b
DIJOUX <emmanuel.dijoux14@gmail.com>
2017-02-28 18:14:18 +0100

    ajout de commentaires et gestion de formulaire

--- app/app.php
+++ app/app.php
@@ -30,6 +30,11 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
     ),
 ));
 
+$app->register(new Silex\Provider\FormServiceProvider());
+$app->register(new Silex\Provider\LocaleServiceProvider());
+$app->register(new Silex\Provider\TranslationServiceProvider());
+
+
 // Register services
 $app['dao.article'] = function ($app) {
     return new MicroCMS\DAO\ArticleDAO($app['db']);
